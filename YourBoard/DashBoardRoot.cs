f50af3b03aa32054d598e43e8a397c4fd619c817
VladS23 <77384665+VladS23@users.noreply.github.com>
2023-01-28 16:32:35 +0300

    button upd

---
+++ YourBoard/DashBoardRoot.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace YourBoard
+{
+    internal class DashBoardRoot
+    {
+        public static double createdPersonX;
+        public static double createdPersonY;
+        public static List<DashBoardElement> Persons = new List<DashBoardElement>();
+        public static Canvas MainCanvas = new Canvas();
+        public static bool isMousePressedinPerson = false;
+        public DashBoardRoot()
+        {
+            MainCanvas.Background = new SolidColorBrush(Color.FromRgb(69, 94, 131));
+            ContextMenu canvasMenu = new ContextMenu();
+            MenuItem createPerson = new MenuItem();
+            createPerson.Header = "Create a person";
+            canvasMenu.Items.Add(createPerson);
+            MainCanvas.ContextMenu = canvasMenu;
+            createPerson.PreviewMouseUp += CreatePersonMenuOpen;
+            MainCanvas.PreviewMouseMove += OnMouseMove;
+            MainCanvas.PreviewMouseLeftButtonUp += OnMouseUp;
+        }
+
+        private void OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void CreatePersonMenuOpen(object sender, MouseButtonEventArgs e)
+        {
+        }
+    }
+}
--- YourBoard/MainWindow.xaml.cs
+++ YourBoard/MainWindow.xaml.cs
@@ -23,6 +23,38 @@ namespace YourBoard
         public MainWindow()
         {
             InitializeComponent();
+            DashBoardRoot dashBoardRoot = new DashBoardRoot();
+            MainGrid.Children.Add(DashBoardRoot.MainCanvas);
+
+            StackPanel view = new StackPanel();
+            StackPanel mystackPanel = new StackPanel();
+            Button myButton = new Button();
+            BitmapImage myBitmapImage = new BitmapImage();
+            Image PersonImage = new Image();
+            myBitmapImage.BeginInit();
+            myBitmapImage.UriSource = new Uri(@"C:\Users\Vlad\Desktop\photo_2023-01-18_13-40-30.jpg");
+            myBitmapImage.EndInit();
+            PersonImage.Source = myBitmapImage;
+            PersonImage.Stretch = Stretch.Fill;
+            //myButton.Content = PersonImage;
+            myButton.Width = 500;
+            myButton.Height = 500;
+            Canvas.SetLeft(mystackPanel, 100);
+            Canvas.SetTop(mystackPanel, 100);
+            mystackPanel.Children.Add(myButton);
+            view = mystackPanel;
+            Canvas.SetZIndex(view, 150);
+            DashBoardRoot.MainCanvas.Children.Add(view);
+
+            LinearGradientBrush gradientBrush = new LinearGradientBrush();
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.LightGray, 0));
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.White, 1));
+
+            // добавление ресурса в словарь ресурсов окна
+            this.Resources.Add("buttonGradientBrush", gradientBrush);
+
+            // установка ресурса у кнопки
+            myButton.Background = (Brush)this.TryFindResource("buttonGradientBrush");
         }
     }
 }
